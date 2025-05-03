45fb840fe62e1fd5b42509bfe6b269871f91de7f
CatGettingHitInTheFace <feartherenegade@gmail.com>
2025-05-03 02:04:05 -0500

    Changed webhistory to webhashhistory to fix link issues

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,15 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomePage    from '../views/HomePage.vue'
 import ArchivePage from '../views/ArchivePage.vue'
 import ContentView from '../views/ContentView.vue'
 import AboutPage   from '../views/AboutPage.vue'
 
-const routes = [
-  { path: '/',            component: HomePage },
-  { path: '/archive',     component: ArchivePage },
-  { path: '/archive/:id', component: ContentView, props: true },
-  { path: '/about',       component: AboutPage }
-]
-
 export default createRouter({
-  history: createWebHistory(),
-  routes
+  history: createWebHashHistory(), 
+  routes: [
+    { path: '/',           component: HomePage   },
+    { path: '/archive',    component: ArchivePage},
+    { path: '/archive/:id',component: ContentView, props: true },
+    { path: '/about',      component: AboutPage  },
+  ]
 })
