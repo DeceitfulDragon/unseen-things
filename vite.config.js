30e0e1b764a944c666e1d257cd771b0b845be1a9
CatGettingHitInTheFace <feartherenegade@gmail.com>
2025-05-03 00:29:06 -0500

    update base & package

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production'
-  ? '/unseen-things/'
-  : '/',
+  base: '/',
   plugins: [vue(), tailwindcss(),],
 })
